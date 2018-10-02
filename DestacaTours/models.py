3d8a0e8036f493abbe49db69944f3f5c6cae7641
javier <javier@fdrb.cl>
2018-10-02 00:25:40 -0300

    more models

--- DestacaTours/models.py
+++ DestacaTours/models.py
@@ -14,11 +14,17 @@ class Bus(models.Model):
 	plate = models.CharField(min_length=6, max_length=6)
 
 
+class Terminal(models.Model):
+	name = models.CharField(max_length=20)
+
+
 class Route(models.Model):
-	#origin
-	#destination
+	origin = models.ForeignKey(Terminal, on_delete=models.CASCADE)
+	destination = models.ForeignKey(Terminal, on_delete=models.CASCADE)
 
 
 class Schedule(models.Model):
 	bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
 	driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+	startTime = models.DateTimeField()
+	blocks = models.IntegerField()
